530c3ed04d3b4ef7e90876c9cb50e14bea89190b
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-05-06 12:15:06 +0200

    refactor errors handling
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- writers/postgres/messages.go
+++ writers/postgres/messages.go
@@ -20,7 +20,8 @@ var (
 	// ErrInvalidMessage indicates that service received message that
 	// doesn't fit required format.
 	ErrInvalidMessage = errors.New("invalid message representation")
-	errSaveMessage    = errors.New("failed to save message to postgress database")
+	errSaveMessage    = errors.New("failed to save message to postgres database")
+	errTransRollback  = errors.New("failed to rollback transaction")
 )
 
 var _ writers.MessageRepository = (*postgresRepo)(nil)
@@ -34,7 +35,7 @@ func New(db *sqlx.DB) writers.MessageRepository {
 	return &postgresRepo{db: db}
 }
 
-func (pr postgresRepo) Save(messages ...senml.Message) error {
+func (pr postgresRepo) Save(messages ...senml.Message) (err error) {
 	q := `INSERT INTO messages (id, channel, subtopic, publisher, protocol,
     name, unit, value, string_value, bool_value, data_value, sum,
     time, update_time)
@@ -44,15 +45,14 @@ func (pr postgresRepo) Save(messages ...senml.Message) error {
 
 	tx, err := pr.db.BeginTxx(context.Background(), nil)
 	defer func() {
-		if err == nil {
-			if err = tx.Commit(); err != nil {
-				err = errors.Wrap(errSaveMessage, err)
+		if err != nil {
+			if txErr := tx.Rollback(); txErr != nil {
+				err = errors.Wrap(errTransRollback, txErr)
 			}
 			return
 		}
-		err = errors.Wrap(errSaveMessage, err)
-		if txErr := tx.Rollback(); txErr != nil {
-			err = errors.Wrap(errSaveMessage, txErr)
+		if err = tx.Commit(); err != nil {
+			err = errors.Wrap(errSaveMessage, err)
 		}
 		return
 	}()
@@ -78,7 +78,6 @@ func (pr postgresRepo) Save(messages ...senml.Message) error {
 			return errors.Wrap(errSaveMessage, err)
 		}
 	}
-
 	return err
 }
 
