1d9da77cefcfc200d12e5a7b1b45db6bfc83d38b
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-07-14 14:27:13 +0200

    add comments
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- certs/pki.go
+++ certs/pki.go
@@ -5,6 +5,8 @@ import (
 )
 
 type PKI interface {
+	// IssueCert issues certificate on PKI
 	IssueCert(cn string, ttl, keyType string, keyBits int) (vault.Cert, error)
+	// Revoke revokes certificate from PKI
 	Revoke(serial string) (vault.Revoke, error)
 }
