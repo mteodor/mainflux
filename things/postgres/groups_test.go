1bf31f64a96e57eb4ba9065e22c6b9cb8e7fe237
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-10-27 16:52:14 +0100

    add groups test
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- things/postgres/groups_test.go
+++ things/postgres/groups_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/mainflux/mainflux/pkg/errors"
 	"github.com/mainflux/mainflux/pkg/uuid"
 	uuidProvider "github.com/mainflux/mainflux/pkg/uuid"
-	"github.com/mainflux/mainflux/things"
 	"github.com/mainflux/mainflux/things/postgres"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -57,17 +56,14 @@ import (
 */
 
 var (
-	metadata = things.Metadata{
-		"field": "value",
-	}
-	wrongMeta = things.Metadata{
-		"wrong": "wrong",
-	}
+	wrongMeta = groups.Metadata{"department": "wrong"}
+	metadata  = groups.Metadata{"department": "IoT"}
 )
 
 func TestGroupsSave(t *testing.T) {
 	dbMiddleware := postgres.NewDatabase(db)
 	groupRepo := postgres.NewGroupRepo(dbMiddleware)
+	cleanAll(t, dbMiddleware)
 
 	ownerID, err := uuidProvider.New().ID()
 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
@@ -80,7 +76,7 @@ func TestGroupsSave(t *testing.T) {
 		Description: "description",
 		ID:          groupID,
 		OwnerID:     ownerID,
-		Name:        "mainflux-no-parent",
+		Name:        "mainflux",
 	}
 
 	groupID, err = uuidProvider.New().ID()
@@ -176,6 +172,7 @@ func TestGroupsSave(t *testing.T) {
 func TestRetrieveByID(t *testing.T) {
 	dbMiddleware := postgres.NewDatabase(db)
 	groupRepo := postgres.NewGroupRepo(dbMiddleware)
+	cleanAll(t, dbMiddleware)
 
 	ownerID, err := uuidProvider.New().ID()
 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
@@ -209,7 +206,7 @@ func TestRetrieveByID(t *testing.T) {
 		{
 			desc:    "retrieve a group with incorrect id",
 			groupID: incorrectID,
-			err:     things.ErrNotFound,
+			err:     groups.ErrNotFound,
 		},
 	}
 
@@ -222,59 +219,32 @@ func TestRetrieveByID(t *testing.T) {
 func TestRetrieveAll(t *testing.T) {
 	dbMiddleware := postgres.NewDatabase(db)
 	groupRepo := postgres.NewGroupRepo(dbMiddleware)
-
+	cleanAll(t, dbMiddleware)
 	ownerID, err := uuidProvider.New().ID()
 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
 
-	groupID, err := uuidProvider.New().ID()
-	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-
-	group := groups.Group{
-		Description: "description",
-		ID:          groupID,
-		OwnerID:     ownerID,
-		Name:        "mainflux-no-parent",
-	}
-
-	_, err = groupRepo.Save(context.Background(), group)
-	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-
-	incorrectID, err := uuidProvider.New().ID()
-	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-
 	up := uuidProvider.New()
-	offset := uint64(1)
 	nameNum := uint64(3)
 	metaNum := uint64(3)
-	nameMetaNum := uint64(2)
 
 	n := uint64(10)
 	for i := uint64(0); i < n; i++ {
-		id, err := up.ID()
+		groupID, err := up.ID()
 		require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-		key, err := up.ID()
-		require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-		th := things.Thing{
-			Owner: email,
-			ID:    id,
-			Key:   key,
+
+		group := groups.Group{
+			Description: "description",
+			ID:          groupID,
+			OwnerID:     ownerID,
+			Name:        fmt.Sprintf("mainflux%d", i),
 		}
 
-		// Create Things with name.
-		if i < nameNum {
-			th.Name = name
-		}
 		// Create Things with metadata.
 		if i >= nameNum && i < nameNum+metaNum {
-			th.Metadata = metadata
-		}
-		// Create Things with name and metadata.
-		if i >= n-nameMetaNum {
-			th.Metadata = metadata
-			th.Name = name
+			group.Metadata = metadata
 		}
 
-		_, err = thingRepo.Save(context.Background(), th)
+		_, err = groupRepo.Save(context.Background(), group)
 		require.Nil(t, err, fmt.Sprintf("unexpected error: %s\n", err))
 	}
 
@@ -286,43 +256,23 @@ func TestRetrieveAll(t *testing.T) {
 		total    uint64
 		metadata map[string]interface{}
 	}{
-		"retrieve all things with existing owner": {
+		"retrieve all groups": {
 			offset: 0,
 			limit:  n,
 			size:   n,
 			total:  n,
 		},
-		"retrieve subset of things with existing owner": {
+		"retrieve subset of groups": {
 			offset: n / 2,
 			limit:  n,
 			size:   n / 2,
 			total:  n,
 		},
-		"retrieve things with non-existing owner": {
-			offset: 0,
-			limit:  n,
-			size:   0,
-			total:  0,
-		},
-		"retrieve things with existing name": {
-			offset: 1,
-			limit:  n,
-			name:   name,
-			size:   nameNum + nameMetaNum - offset,
-			total:  nameNum + nameMetaNum,
-		},
-		"retrieve things with non-existing name": {
-			offset: 0,
-			limit:  n,
-			name:   "wrong",
-			size:   0,
-			total:  0,
-		},
 		"retrieve things with existing metadata": {
 			offset:   0,
 			limit:    n,
-			size:     metaNum + nameMetaNum,
-			total:    metaNum + nameMetaNum,
+			size:     metaNum,
+			total:    metaNum,
 			metadata: metadata,
 		},
 		"retrieve things with non-existing metadata": {
@@ -331,14 +281,6 @@ func TestRetrieveAll(t *testing.T) {
 			size:     0,
 			total:    0,
 			metadata: wrongMeta,
-		},
-		"retrieve all things with existing name and metadata": {
-			offset:   0,
-			limit:    n,
-			size:     nameMetaNum,
-			total:    nameMetaNum,
-			name:     name,
-			metadata: metadata,
 		},
 	}
 
@@ -354,203 +296,195 @@ func TestRetrieveAll(t *testing.T) {
 func TestGroupDelete(t *testing.T) {
 	dbMiddleware := postgres.NewDatabase(db)
 	repo := postgres.NewGroupRepo(dbMiddleware)
-	uid, err := uuid.New().ID()
+	cleanAll(t, dbMiddleware)
+	gid, err := uuid.New().ID()
 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-	user := things.User{
-		ID:       uid,
-		Email:    "TestGroupDelete@mainflux.com",
-		Password: password,
+
+	ownerID, err := uuid.New().ID()
+	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
+
+	group := groups.Group{
+		Description: "description",
+		ID:          gid,
+		OwnerID:     ownerID,
+		Name:        "mainflux-no-parent",
 	}
-	_, err = userRepo.Save(context.Background(), user)
+	_, err = repo.Save(context.Background(), group)
 	require.Nil(t, err, fmt.Sprintf("save got unexpected error: %s", err))
 
-	user, err = userRepo.RetrieveByEmail(context.Background(), user.Email)
-	require.Nil(t, err, fmt.Sprintf("retrieve got unexpected error: %s", err))
-
-	gid, err := uuid.New().ID()
-	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
-	group1 := groups.Group{
-		ID:      gid,
-		Name:    groupName + "TestGroupDelete1",
-		OwnerID: user.ID,
-	}
-
-	g1, err := repo.Save(context.Background(), group1)
-	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
-
-	err = repo.Assign(context.Background(), user.ID, g1.ID)
-	require.Nil(t, err, fmt.Sprintf("failed to assign user to a group: %s", err))
-
-	gid, err = uuid.New().ID()
-	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
-	group2 := groups.Group{
-		ID:      gid,
-		Name:    groupName + "TestGroupDelete2",
-		OwnerID: user.ID,
-	}
-
-	g2, err := repo.Save(context.Background(), group2)
-	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
+	gidNonExisting, err := uuid.New().ID()
+	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
 
 	cases := []struct {
-		desc  string
-		group groups.Group
-		err   error
+		desc    string
+		groupID string
+		err     error
 	}{
 		{
-			desc:  "delete group for existing id",
-			group: g2,
-			err:   nil,
+			desc:    "delete group for existing id",
+			groupID: gid,
+			err:     nil,
 		},
 		{
-			desc:  "delete group for non-existing id",
-			group: g2,
-			err:   things.ErrDeleteGroupMissing,
+			desc:    "delete group for non-existing id",
+			groupID: gidNonExisting,
+			err:     groups.ErrDeleteGroup,
 		},
 	}
 
 	for _, tc := range cases {
-		err := repo.Delete(context.Background(), tc.group.ID)
+		err := repo.Delete(context.Background(), tc.groupID)
 		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s: expected %s got %s\n", tc.desc, tc.err, err))
 	}
 }
 
-func TestAssignUser(t *testing.T) {
-	dbMiddleware := postgres.NewDatabase(db)
-	repo := postgres.NewGroupRepo(dbMiddleware)
-	userRepo := postgres.NewUserRepo(dbMiddleware)
-	uid, err := uuid.New().ID()
+// func TestAssignUser(t *testing.T) {
+// 	dbMiddleware := postgres.NewDatabase(db)
+// 	repo := postgres.NewGroupRepo(dbMiddleware)
+// 	userRepo := postgres.NewUserRepo(dbMiddleware)
+// 	uid, err := uuid.New().ID()
+// 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
+// 	user := things.User{
+// 		ID:       uid,
+// 		Email:    "TestAssignUser@mainflux.com",
+// 		Password: password,
+// 	}
+
+// 	_, err = userRepo.Save(context.Background(), user)
+// 	require.Nil(t, err, fmt.Sprintf("save got unexpected error: %s", err))
+
+// 	user, err = userRepo.RetrieveByEmail(context.Background(), user.Email)
+// 	require.Nil(t, err, fmt.Sprintf("retrieve got unexpected error: %s", err))
+
+// 	gid, err := uuid.New().ID()
+// 	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
+// 	group1 := groups.Group{
+// 		ID:      gid,
+// 		Name:    groupName + "TestAssignUser1",
+// 		OwnerID: user.ID,
+// 	}
+
+// 	g1, err := repo.Save(context.Background(), group1)
+// 	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
+
+// 	gid, err = uuid.New().ID()
+// 	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
+// 	group2 := groups.Group{
+// 		ID:      gid,
+// 		Name:    groupName + "TestAssignUser2",
+// 		OwnerID: user.ID,
+// 	}
+
+// 	g2, err := repo.Save(context.Background(), group2)
+// 	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
+
+// 	gid, err = uuidProvider.New().ID()
+// 	require.Nil(t, err, fmt.Sprintf("group id generating error: %s", err))
+// 	g3 := groups.Group{
+// 		ID: gid,
+// 	}
+
+// 	cases := []struct {
+// 		desc  string
+// 		group groups.Group
+// 		err   error
+// 	}{
+// 		{
+// 			desc:  "assign user to existing group",
+// 			group: g1,
+// 			err:   nil,
+// 		},
+// 		{
+// 			desc:  "assign user to another existing group",
+// 			group: g2,
+// 			err:   nil,
+// 		},
+// 		{
+// 			desc:  "assign user to non existing group",
+// 			group: g3,
+// 			err:   things.ErrNotFound,
+// 		},
+// 	}
+
+// 	for _, tc := range cases {
+// 		err := repo.Assign(context.Background(), user.ID, tc.group.ID)
+// 		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s: expected %s got %s\n", tc.desc, tc.err, err))
+// 	}
+
+// }
+
+// func TestUnassignUser(t *testing.T) {
+// 	dbMiddleware := postgres.NewDatabase(db)
+// 	repo := postgres.NewGroupRepo(dbMiddleware)
+// 	userRepo := postgres.NewUserRepo(dbMiddleware)
+
+// 	uid, err := uuid.New().ID()
+// 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
+// 	user := things.User{
+// 		ID:       uid,
+// 		Email:    "UnassignUser1@mainflux.com",
+// 		Password: password,
+// 	}
+
+// 	_, err = userRepo.Save(context.Background(), user)
+// 	require.Nil(t, err, fmt.Sprintf("save got unexpected error: %s", err))
+
+// 	user1, err := userRepo.RetrieveByEmail(context.Background(), user.Email)
+// 	require.Nil(t, err, fmt.Sprintf("retrieve got unexpected error: %s", err))
+
+// 	uid, err = uuid.New().ID()
+// 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
+// 	user = things.User{
+// 		ID:       uid,
+// 		Email:    "UnassignUser2@mainflux.com",
+// 		Password: password,
+// 	}
+
+// 	_, err = userRepo.Save(context.Background(), user)
+// 	require.Nil(t, err, fmt.Sprintf("save got unexpected error: %s", err))
+
+// 	user2, err := userRepo.RetrieveByEmail(context.Background(), user.Email)
+// 	require.Nil(t, err, fmt.Sprintf("retrieve got unexpected error: %s", err))
+
+// 	gid, err := uuid.New().ID()
+// 	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
+// 	group1 := groups.Group{
+// 		ID:      gid,
+// 		Name:    groupName + "UnassignUser1",
+// 		OwnerID: user.ID,
+// 	}
+
+// 	g1, err := repo.Save(context.Background(), group1)
+// 	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
+
+// 	err = repo.Assign(context.Background(), user1.ID, group1.ID)
+// 	require.Nil(t, err, fmt.Sprintf("failed to assign user: %s", err))
+
+// 	cases := []struct {
+// 		desc  string
+// 		group groups.Group
+// 		user  things.User
+// 		err   error
+// 	}{
+// 		{desc: "remove user from a group", group: g1, user: user1, err: nil},
+// 		{desc: "remove already removed user from a group", group: g1, user: user1, err: nil},
+// 		{desc: "remove non existing user from a group", group: g1, user: user2, err: nil},
+// 	}
+
+// 	for _, tc := range cases {
+// 		err := repo.Unassign(context.Background(), tc.user.ID, tc.group.ID)
+// 		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s: expected %s got %s\n", tc.desc, tc.err, err))
+// 	}
+
+// }
+func cleanAll(t *testing.T, db postgres.Database) {
+	params := map[string]interface{}{}
+	rows, err := db.NamedQueryContext(context.Background(), "delete from thing_groups;", params)
 	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-	user := things.User{
-		ID:       uid,
-		Email:    "TestAssignUser@mainflux.com",
-		Password: password,
+	defer rows.Close()
+	total := uint64(0)
+	if rows.Next() {
+		err := rows.Scan(&total)
+		require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
 	}
-
-	_, err = userRepo.Save(context.Background(), user)
-	require.Nil(t, err, fmt.Sprintf("save got unexpected error: %s", err))
-
-	user, err = userRepo.RetrieveByEmail(context.Background(), user.Email)
-	require.Nil(t, err, fmt.Sprintf("retrieve got unexpected error: %s", err))
-
-	gid, err := uuid.New().ID()
-	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
-	group1 := groups.Group{
-		ID:      gid,
-		Name:    groupName + "TestAssignUser1",
-		OwnerID: user.ID,
-	}
-
-	g1, err := repo.Save(context.Background(), group1)
-	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
-
-	gid, err = uuid.New().ID()
-	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
-	group2 := groups.Group{
-		ID:      gid,
-		Name:    groupName + "TestAssignUser2",
-		OwnerID: user.ID,
-	}
-
-	g2, err := repo.Save(context.Background(), group2)
-	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
-
-	gid, err = uuidProvider.New().ID()
-	require.Nil(t, err, fmt.Sprintf("group id generating error: %s", err))
-	g3 := groups.Group{
-		ID: gid,
-	}
-
-	cases := []struct {
-		desc  string
-		group groups.Group
-		err   error
-	}{
-		{
-			desc:  "assign user to existing group",
-			group: g1,
-			err:   nil,
-		},
-		{
-			desc:  "assign user to another existing group",
-			group: g2,
-			err:   nil,
-		},
-		{
-			desc:  "assign user to non existing group",
-			group: g3,
-			err:   things.ErrNotFound,
-		},
-	}
-
-	for _, tc := range cases {
-		err := repo.Assign(context.Background(), user.ID, tc.group.ID)
-		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s: expected %s got %s\n", tc.desc, tc.err, err))
-	}
-
-}
-
-func TestUnassignUser(t *testing.T) {
-	dbMiddleware := postgres.NewDatabase(db)
-	repo := postgres.NewGroupRepo(dbMiddleware)
-	userRepo := postgres.NewUserRepo(dbMiddleware)
-
-	uid, err := uuid.New().ID()
-	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-	user := things.User{
-		ID:       uid,
-		Email:    "UnassignUser1@mainflux.com",
-		Password: password,
-	}
-
-	_, err = userRepo.Save(context.Background(), user)
-	require.Nil(t, err, fmt.Sprintf("save got unexpected error: %s", err))
-
-	user1, err := userRepo.RetrieveByEmail(context.Background(), user.Email)
-	require.Nil(t, err, fmt.Sprintf("retrieve got unexpected error: %s", err))
-
-	uid, err = uuid.New().ID()
-	require.Nil(t, err, fmt.Sprintf("got unexpected error: %s", err))
-	user = things.User{
-		ID:       uid,
-		Email:    "UnassignUser2@mainflux.com",
-		Password: password,
-	}
-
-	_, err = userRepo.Save(context.Background(), user)
-	require.Nil(t, err, fmt.Sprintf("save got unexpected error: %s", err))
-
-	user2, err := userRepo.RetrieveByEmail(context.Background(), user.Email)
-	require.Nil(t, err, fmt.Sprintf("retrieve got unexpected error: %s", err))
-
-	gid, err := uuid.New().ID()
-	require.Nil(t, err, fmt.Sprintf("group id unexpected error: %s", err))
-	group1 := groups.Group{
-		ID:      gid,
-		Name:    groupName + "UnassignUser1",
-		OwnerID: user.ID,
-	}
-
-	g1, err := repo.Save(context.Background(), group1)
-	require.Nil(t, err, fmt.Sprintf("group save got unexpected error: %s", err))
-
-	err = repo.Assign(context.Background(), user1.ID, group1.ID)
-	require.Nil(t, err, fmt.Sprintf("failed to assign user: %s", err))
-
-	cases := []struct {
-		desc  string
-		group groups.Group
-		user  things.User
-		err   error
-	}{
-		{desc: "remove user from a group", group: g1, user: user1, err: nil},
-		{desc: "remove already removed user from a group", group: g1, user: user1, err: nil},
-		{desc: "remove non existing user from a group", group: g1, user: user2, err: nil},
-	}
-
-	for _, tc := range cases {
-		err := repo.Unassign(context.Background(), tc.user.ID, tc.group.ID)
-		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s: expected %s got %s\n", tc.desc, tc.err, err))
-	}
-
 }
