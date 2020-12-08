dbd27b75ecd0a9442f9ef6c1b5ec77c8f40c3c14
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-12-08 16:45:20 +0100

    fix path, group saving with parent
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- things/postgres/groups.go
+++ things/postgres/groups.go
@@ -42,10 +42,11 @@ func NewGroupRepo(db Database) groups.Repository {
 func (gr groupRepository) Save(ctx context.Context, g groups.Group) (groups.Group, error) {
 	var id string
 	q := `INSERT INTO thing_groups (name, description, id, owner_id, metadata, path, created_at, updated_at) 
-		  VALUES (:name, :description, :id, :owner_id, :metadata, :name, now(), now()) RETURNING id`
+		  VALUES (:name, :description, :id, :owner_id, :metadata, CAST(REPLACE(CAST(tg.id AS TEXT),'-','_') as ltree), now(), now() RETURNING id`
 	if g.ParentID != "" {
-		q = `INSERT INTO thing_groups (name, description, id, owner_id, parent_id, metadata, path) 
-			 SELECT :name, :description, :id, :owner_id, :parent_id, :metadata, text2ltree(ltree2text(tg.path) || '.' || :name) FROM thing_groups tg WHERE id = :parent_id RETURNING id`
+		q = `INSERT INTO thing_groups (name, description, id, owner_id, parent_id, metadata, path, created_at, updated_at) 
+			 SELECT :name, :description, :id, :owner_id, :parent_id, :metadata, text2ltree(ltree2text(tg.path) || '.' || REPLACE(CAST(:id AS TEXT),'-','_')), now(), now() 
+			 FROM thing_groups tg WHERE id = :parent_id RETURNING id`
 	}
 
 	dbu, err := toDBGroup(g)
