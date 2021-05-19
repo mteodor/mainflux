8033215a34136d77d384dbb257654525e89253ce
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2021-05-19 17:13:15 +0200

    replace nginx with openresty
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -2,11 +2,11 @@
 
 if [ -z "$MF_MQTT_CLUSTER" ]
 then
-      envsubst '${MF_MQTT_ADAPTER_MQTT_PORT}' < /etc/nginx/snippets/mqtt-upstream-single.conf > /etc/nginx/snippets/mqtt-upstream.conf
-      envsubst '${MF_MQTT_ADAPTER_WS_PORT}' < /etc/nginx/snippets/mqtt-ws-upstream-single.conf > /etc/nginx/snippets/mqtt-ws-upstream.conf
+      envsubst '${MF_MQTT_ADAPTER_MQTT_PORT}' < /usr/local/openresty/nginx/conf/snippets/mqtt-upstream-single.conf > /usr/local/openresty/nginx/conf/snippets/mqtt-upstream.conf
+      envsubst '${MF_MQTT_ADAPTER_WS_PORT}' < /usr/local/openresty/nginx/conf/snippets/mqtt-ws-upstream-single.conf > /usr/local/openresty/nginx/conf/snippets/mqtt-ws-upstream.conf
 else
-      envsubst '${MF_MQTT_ADAPTER_MQTT_PORT}' < /etc/nginx/snippets/mqtt-upstream-cluster.conf > /etc/nginx/snippets/mqtt-upstream.conf
-      envsubst '${MF_MQTT_ADAPTER_WS_PORT}' < /etc/nginx/snippets/mqtt-ws-upstream-cluster.conf > /etc/nginx/snippets/mqtt-ws-upstream.conf
+      envsubst '${MF_MQTT_ADAPTER_MQTT_PORT}' < /usr/local/openresty/nginx/conf/snippets/mqtt-upstream-cluster.conf > /usr/local/openresty/nginx/conf/snippets/mqtt-upstream.conf
+      envsubst '${MF_MQTT_ADAPTER_WS_PORT}' < /usr/local/openresty/nginx/conf/snippets/mqtt-ws-upstream-cluster.conf > /usr/local/openresty/nginx/conf/snippets/mqtt-ws-upstream.conf
 fi
 
 envsubst '
@@ -17,6 +17,12 @@ envsubst '
     ${MF_NGINX_MQTT_PORT}
     ${MF_NGINX_MQTTS_PORT}
     ${MF_AUTH_HTTP_PORT}
-    ${MF_WS_ADAPTER_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+    ${MF_WS_ADAPTER_PORT}
+    ${MF_UI_PORT}
+    ${MF_INFLUX_READER_PORT}
+    ${MF_BOOTSTRAP_PORT}
+    ${MF_TWINS_HTTP_PORT}
+    ${MF_OPCUA_ADAPTER_HTTP_PORT}' < /usr/local/openresty/nginx/conf/nginx.conf.template > /usr/local/openresty/nginx/conf/mfx-nginx.conf
 
-exec nginx -g "daemon off;"
+
+exec /usr/local/openresty/bin/openresty -g "daemon off;" 
